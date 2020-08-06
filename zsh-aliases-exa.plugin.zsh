d01b56526ccf603c2d0d59412948b137f6b77377
Nikita Klimenko <40723536+n12o@users.noreply.github.com>
2020-08-07 00:58:22 +0200

    Update zsh-aliases-exa.plugin.zsh

--- zsh-aliases-exa.plugin.zsh
+++ zsh-aliases-exa.plugin.zsh
@@ -12,8 +12,8 @@
 alias ls='exa'                                                         # ls
 alias l='exa -lbF --git'                                               # list, size, type, git
 alias ll='exa -lbGF --git'                                             # long list
-alias llm='exa -lbGF --git --sort=modified'                            # long list, modified date sort
-alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
+alias llm='exa -lbGF --git --sort=modified'                             # long list, modified date sort
+alias la='exa -a'                                                      # hidden
 alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
 
 # speciality views
